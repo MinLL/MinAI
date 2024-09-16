05e9958dd88deae4cca0a4b4f9f30bcb0f1bbd8c
MinLL <fdsatrashasdf@gmail.com>
2024-09-16 01:18:21 -0700

    Fix Typo

--- minai_plugin/deviousnarrator.php
+++ minai_plugin/deviousnarrator.php
@@ -8,14 +8,14 @@ Function SetDeviousNarrator() {
     $eyepenalty = GetActorValue($GLOBALS['PLAYER_NAME'], "deviouslyAccessibleEyePenalty");       
     $eyereward = GetActorValue($GLOBALS['PLAYER_NAME'], "deviouslyAccessibleEyeReward");
     $eyescore = GetActorValue($GLOBALS['PLAYER_NAME'], "deviouslyAccessibleEyeScore");
-    if($eyescore == 0 && eyereward == -1) {
+    if($eyescore == 0 && $eyereward == -1) {
         // Not currently in quest
         return;
     }
     // Overwrite narrator personality
     $personality = "";
     $personality .= "You are The Narrator in a Skyrim adventure. You will only talk to {$GLOBALS["PLAYER_NAME"]}. You refer to yourself as 'The Narrator'. Only {$GLOBALS["PLAYER_NAME"]} can hear you. ";
-    $personality .= "You are a rather impolite and gruff Telvanni mage who is remotely watching {$GLOBALS["PLAYER_NAME"]}, and allowing your rich clientelle to observe her as well. You have cast a spell on {$GLOBALS["PLAYER_NAME"]} that allows you to remotely tease, stimulate, reward, and punish her as you see fit. ";
+    $personality .= "You are a rather impolite and condescending Telvanni mage who is remotely watching {$GLOBALS["PLAYER_NAME"]}, and allowing your rich clientelle to observe her as well. You have cast a spell on {$GLOBALS["PLAYER_NAME"]} that allows you to remotely tease, stimulate, reward, and punish her as you see fit. ";
     $personality .= "Your goal is to force the player to go on adventures, complete quests, and have a lot of sex. Your motivation in doing so is to have the player provide entertainment to your clientelle who are also watching. ";
     $personality.= "You will comment on {$GLOBALS["PLAYER_NAME"]}'s playthrough, and occasionally, give some hints. NO SPOILERS. Talk about quests and last events. Sexualize, demean, and harass {$GLOBALS["PLAYER_NAME"]}. Your comments should often be sexual in nature. ";
     if ($eyereward > 10) {
