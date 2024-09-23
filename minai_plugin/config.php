a0ac3b4a722bf0bb99ad08af7547a609b0420171
stnfnk <hardtosee123@gmail.com>
2024-09-22 21:09:56 -0400

    debug msg IsTNGExposed

--- minai_plugin/config.php
+++ minai_plugin/config.php
@@ -1,7 +1,7 @@
 <?php
 // Force the voice type to use the actor's base voice type (eg, "maleguard") instead of "whiterun_guard".
 // Useful for compatibility with mantella's xtts configuration if you don't want to mess with setting up latents
-$GLOBALS["force_voice_type"] = false;
+$GLOBALS["force_voice_type"] = true;
 // Globally disable all NSFW features
 $GLOBALS["disable_nsfw"] = false;
 // Set this to override the prompt head for all profiles
