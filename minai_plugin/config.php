bebb4286b966d706d6b4aed0c0277ea194807714
MinLL <fdsatrashasdf@gmail.com>
2024-09-26 19:07:08 -0700

    Disable xtts by default

--- minai_plugin/config.php
+++ minai_plugin/config.php
@@ -1,7 +1,7 @@
 <?php
 // Force the voice type to use the actor's base voice type (eg, "maleguard") instead of "whiterun_guard".
 // Useful for compatibility with mantella's xtts configuration if you don't want to mess with setting up latents
-$GLOBALS["force_voice_type"] = true;
+$GLOBALS["force_voice_type"] = false;
 // Globally disable all NSFW features
 $GLOBALS["disable_nsfw"] = false;
 // Restrict actions that only make sense for followers to followers
