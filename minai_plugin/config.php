2a0cca7687ffcc03495585cc6d8c60e7f4cce017
MinLL <fdsatrashasdf@gmail.com>
2024-10-10 23:58:56 -0700

    Update config.php

--- minai_plugin/config.php
+++ minai_plugin/config.php
@@ -1,4 +1,8 @@
 <?php
+// Player bio - Added to the prompt head for dialogue that is not npc -> npc
+$GLOBALS["PLAYER_BIO"] = "I'm #PLAYER_NAME#. ";
+// Set this to override the prompt head for all profiles
+$GLOBALS["PROMPT_HEAD_OVERRIDE"] = "";
 // Force the voice type to use the actor's base voice type (eg, "maleguard") instead of "whiterun_guard".
 // Useful for compatibility with mantella's xtts configuration if you don't want to mess with setting up latents
 $GLOBALS["force_voice_type"] = false;
@@ -11,8 +15,6 @@ $GLOBALS["stop_narrator_context_leak"] = false;
 // Enable functions during things like rechat.
 // Allows for better party dynamics. Disabled for the narrator to avoid CTD.
 $GLOBALS["always_enable_functions"] = true;
-// Set this to override the prompt head for all profiles
-$GLOBALS["PROMPT_HEAD_OVERRIDE"] = "";
 // Force the configured name in AI-FF to match the in-game name
 $GLOBALS["force_aiff_name_to_ingame_name"] = true;
 // List of commands to disable.
