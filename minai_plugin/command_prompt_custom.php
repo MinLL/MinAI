91cc2b4a374d85a23cd7f60b06c77f2d42a308a7
MinLL <fdsatrashasdf@gmail.com>
2024-10-14 14:15:37 -0700

    Add support for merging override + command prompt

--- minai_plugin/command_prompt_custom.php
+++ minai_plugin/command_prompt_custom.php
@@ -3,12 +3,21 @@ require_once("util.php");
 $GLOBALS["PATCH_PROMPT_ENFORCE_ACTIONS"] = true;
 $target = GetTargetActor();
 
+function SetPromptHead($override) {
+    if (str_starts_with($GLOBALS["PROMPT_HEAD"], "#")) {
+        $GLOBALS["PROMPT_HEAD"] = "{$override} {$GLOBALS["PROMPT_HEAD"]}";
+    }
+    else {
+        $GLOBALS["PROMPT_HEAD"] = $override;
+    }
+}
+
 $GLOBALS["COMMAND_PROMPT_ENFORCE_ACTIONS"]="Choose a coherent ACTION that is available to you in order to obey or physically interact with {$target}. You can also use an ACTION to interact with the world, provide services, or indicate your arousal. ";
 $shouldOverride = ($GLOBALS["PROMPT_HEAD_OVERRIDE"] != "" && isset($GLOBALS["PROMPT_HEAD_OVERRIDE"]));
 
 if (IsRadiant()) { // Is this npc -> npc?
     if ($shouldOverride) // Override prompt head
-        $GLOBALS["PROMPT_HEAD"] = $GLOBALS["PROMPT_HEAD_OVERRIDE"];
+        SetPromptHead($GLOBALS["PROMPT_HEAD_OVERRIDE"]);
     else {
         // No need to do anything
     }
@@ -16,9 +25,9 @@ if (IsRadiant()) { // Is this npc -> npc?
 }
 else {
     if ($shouldOverride)
-        $GLOBALS["PROMPT_HEAD"] = $GLOBALS["PLAYER_BIO"] . " " . $GLOBALS["PROMPT_HEAD_OVERRIDE"];
+        SetPromptHead($GLOBALS["PLAYER_BIO"] . " " . $GLOBALS["PROMPT_HEAD_OVERRIDE"]);
     else
-        $GLOBALS["PROMPT_HEAD"] = $GLOBALS["PLAYER_BIO"] . " " . $GLOBALS["PROMPT_HEAD"];
+        SetPromptHead($GLOBALS["PLAYER_BIO"] . " " . $GLOBALS["PROMPT_HEAD"]);
 }
 
 ?>
