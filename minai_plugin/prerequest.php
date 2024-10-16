42a39b8541a1448870a6bd8c1c45aad6a9a1719f
MinLL <fdsatrashasdf@gmail.com>
2024-10-15 20:34:55 -0700

    Add support for "The Narrator" profile

--- minai_plugin/prerequest.php
+++ minai_plugin/prerequest.php
@@ -8,6 +8,22 @@ Function SetRadiance($rechat_h, $rechat_p) {
     $GLOBALS["RECHAT_P"] = $rechat_p;
 }
 
+Function GetNarratorConfigPath() {
+    $path = dirname((__FILE__)) . DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR;
+    $newConfFile=md5("The Narrator");
+    return $path . "conf".DIRECTORY_SEPARATOR."conf_$newConfFile.php";
+}
+Function SetNarratorProfile() {
+    if (!file_exists(GetNarratorConfigPath())) {
+        error_log("minai: Initializing Narrator Profile");
+        createProfile("The Narrator");
+    }
+    if ($GLOBALS["HERIKA_NAME"] == "The Narrator" && $GLOBALS["use_narrator_profile"]) {
+        error_log("minai: Overwriting profile with narrator profile.");
+        require_once(GetNarratorConfigPath());
+    }
+}
+
 if (IsNewRadiantConversation()) {
     error_log("minai: Initial radiant conversation, overriding rechat parameters");
     $GLOBALS["db"]->delete("conf_opts", "id='_minai_RADIANT//initial'");
@@ -17,9 +33,6 @@ elseif (IsRadiant()) {
     SetRadiance($GLOBALS["radiance_rechat_h"], $GLOBALS["radiance_rechat_p"]);
 }
 
-    
-
-
-
+SetNarratorProfile();
 
 ?>
