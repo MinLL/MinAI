5ba8217da847cc87f09e4eccbdb4e7a1112c527e
Nick Welp <nickwelp@gmail.com>
2025-01-13 10:20:44 -0600

    capitalization is hard

--- minai_plugin/dirtandblood.php
+++ minai_plugin/dirtandblood.php
@@ -1,19 +1,18 @@
 <?php
 require_once("util.php");
-
 // The script is Structured this way to that it will be easier to implement the playmate part.
 // I never played with one so far, so I am not able to do it from the get go.
 function GetDirtAndBloodContext($name) {
+    $utilities = new Utilities();
     if (!$utilities->IsModEnabled("DirtAndBlood")) {
         return "";
     }
     $description = $utilities->GetActorValue($name, "dirtAndBlood");
-    $lowerCaseName = strtolower($name);
-    $result = str_replace($lowerCaseName, ucwords($name), $description);
+    // the names are complicated, just let the llm handle upper casing
     if(!$description) {
         return "";
     }
-    return " ". $result . " ";
+    return " ". $description . " ";
 }
 
 
