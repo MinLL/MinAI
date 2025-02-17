67a75924e4075cdef85775d6fb04ba804df9b3c2
MinLL <fdsatrashasdf@gmail.com>
2025-02-16 16:47:59 -0800

    Fix hardcoded gender references

--- minai_plugin/json_response_custom.php
+++ minai_plugin/json_response_custom.php
@@ -4,9 +4,10 @@ require_once("config.php");
 if (IsEnabled($GLOBALS["PLAYER_NAME"], "isSinging")) {
     $moods=explode(",",$GLOBALS["EMOTEMOODS"]);
     shuffle($moods);
+    $pronouns = GetActorPronouns($GLOBALS["PLAYER_NAME"]);
     $GLOBALS["responseTemplate"] = [
         "character"=>$GLOBALS["PLAYER_NAME"],
-        "listener"=>"{$GLOBALS['PLAYER_NAME']} is singing to those around her",
+        "listener"=>"{$GLOBALS['PLAYER_NAME']} is singing to those around {$pronouns['object']}",
         "message"=>"lines of dialogue",
         "mood"=>implode("|",$moods),
         "action"=>implode("|",$GLOBALS["FUNC_LIST"]),
@@ -17,11 +18,12 @@ if (IsEnabled($GLOBALS["PLAYER_NAME"], "isSinging")) {
 elseif (isset($GLOBALS["self_narrator"]) && $GLOBALS["self_narrator"] && $GLOBALS["HERIKA_NAME"] == "The Narrator") {
     $moods=explode(",",$GLOBALS["EMOTEMOODS"]);
     shuffle($moods);
+    $pronouns = GetActorPronouns($GLOBALS["PLAYER_NAME"]);
     $GLOBALS["responseTemplate"] = [
         "character"=>IsExplicitScene() ? $GLOBALS["PLAYER_NAME"] . "'s body" : $GLOBALS["PLAYER_NAME"] . "'s subconscious",
         "listener"=>IsExplicitScene() ? 
             "{$GLOBALS['PLAYER_NAME']} is reacting to physical sensations" : 
-            "{$GLOBALS['PLAYER_NAME']} is thinking to herself",
+            "{$GLOBALS['PLAYER_NAME']} is thinking to {$pronouns['object']}self",
         "message"=>"lines of dialogue",
         "mood"=>implode("|",$moods),
         "action"=>implode("|",$GLOBALS["FUNC_LIST"]),
