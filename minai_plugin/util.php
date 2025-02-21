866ef436053ad219a70c7aba2892a79f51d671b0
MinLL <fdsatrashasdf@gmail.com>
2025-02-20 19:44:15 -0800

    Fix narrator profile on subsequent calls

--- minai_plugin/util.php
+++ minai_plugin/util.php
@@ -1047,6 +1047,8 @@ Function GetNarratorConfigPath() {
 }
 
 Function SetNarratorProfile() {
+    static $narratorProfileCache = null;
+
     if ($GLOBALS["HERIKA_NAME"] == "The Narrator" && $GLOBALS["use_narrator_profile"]) {
         if (!file_exists(GetNarratorConfigPath())) {
             minai_log("info", "Initializing Narrator Profile");
@@ -1055,36 +1057,77 @@ Function SetNarratorProfile() {
                 "HERIKA_PERS" => "You are The Narrator in a Skyrim adventure. You will only talk to #PLAYER_NAME#. You refer to yourself as 'The Narrator'. Only #PLAYER_NAME# can hear you. Your goal is to comment on #PLAYER_NAME#'s playthrough, and occasionally, give some hints. NO SPOILERS. Talk about quests and last events."
             ], true);
         }
-        $path = GetNarratorConfigPath();
-        // minai_log("info", "Overwriting profile with narrator profile ($path).");
-        // Ignore narrator name
-        global $HERIKA_NAME;
-        $HERIKA_NAME = "The Narrator";
-        global $PROMPT_HEAD;
-        global $PLAYER_BIOS;
-        global $HERIKA_PERS;
-        global $HERIKA_DYNAMIC;
-        global $DYNAMIC_PROFILE;
-        global $RECHAT_H;
-        global $RECHAT_P;
-        global $BORED_EVENT;
-        global $CONTEXT_HISTORY;
-        global $HTTP_TIMEOUT;
-        global $CORE_LANG;
-        global $MAX_WORDS_LIMIT;
-        global $BOOK_EVENT_FULL;
-        global $LANG_LLM_XTTS;
-        global $HERIKA_ANIMATIONS;
-        global $EMOTEMOODS;
-        global $CONNECTORS;
-        global $CONNECTORS_DIARY;
-        global $CONNECTOR;
-        global $TTSFUNCTION;
-        global $TTS;
-        global $STT;
-        global $ITT;
+
+        // If we haven't loaded the narrator profile yet, load it from file
+        if ($narratorProfileCache === null) {
+            // First time load - get the profile from file
+            global $HERIKA_NAME;
+            global $PROMPT_HEAD;
+            global $PLAYER_BIOS;
+            global $HERIKA_PERS;
+            global $HERIKA_DYNAMIC;
+            global $DYNAMIC_PROFILE;
+            global $RECHAT_H;
+            global $RECHAT_P;
+            global $BORED_EVENT;
+            global $CONTEXT_HISTORY;
+            global $HTTP_TIMEOUT;
+            global $CORE_LANG;
+            global $MAX_WORDS_LIMIT;
+            global $BOOK_EVENT_FULL;
+            global $LANG_LLM_XTTS;
+            global $HERIKA_ANIMATIONS;
+            global $EMOTEMOODS;
+            global $CONNECTORS;
+            global $CONNECTORS_DIARY;
+            global $CONNECTOR;
+            global $TTSFUNCTION;
+            global $TTS;
+            global $STT;
+            global $ITT;
         global $FEATURES;
-        require_once($path);
+            $path = GetNarratorConfigPath();
+            $_GET["profile"] = md5("Narrator");
+            require_once($path);
+
+            // Store the loaded profile in cache
+            $narratorProfileCache = [
+                'HERIKA_NAME' => "The Narrator", // Always use "The Narrator"
+                'PROMPT_HEAD' => $GLOBALS['PROMPT_HEAD'],
+                'PLAYER_BIOS' => $GLOBALS['PLAYER_BIOS'],
+                'HERIKA_PERS' => $GLOBALS['HERIKA_PERS'],
+                'HERIKA_DYNAMIC' => $GLOBALS['HERIKA_DYNAMIC'],
+                'DYNAMIC_PROFILE' => $GLOBALS['DYNAMIC_PROFILE'],
+                'RECHAT_H' => $GLOBALS['RECHAT_H'],
+                'RECHAT_P' => $GLOBALS['RECHAT_P'],
+                'BORED_EVENT' => $GLOBALS['BORED_EVENT'],
+                'CONTEXT_HISTORY' => $GLOBALS['CONTEXT_HISTORY'],
+                'HTTP_TIMEOUT' => $GLOBALS['HTTP_TIMEOUT'],
+                'CORE_LANG' => $GLOBALS['CORE_LANG'],
+                'MAX_WORDS_LIMIT' => $GLOBALS['MAX_WORDS_LIMIT'],
+                'BOOK_EVENT_FULL' => $GLOBALS['BOOK_EVENT_FULL'],
+                'LANG_LLM_XTTS' => $GLOBALS['LANG_LLM_XTTS'],
+                'HERIKA_ANIMATIONS' => $GLOBALS['HERIKA_ANIMATIONS'],
+                'EMOTEMOODS' => $GLOBALS['EMOTEMOODS'],
+                'CONNECTORS' => $GLOBALS['CONNECTORS'],
+                'CONNECTORS_DIARY' => $GLOBALS['CONNECTORS_DIARY'],
+                'CONNECTOR' => $GLOBALS['CONNECTOR'],
+                'TTSFUNCTION' => $GLOBALS['TTSFUNCTION'],
+                'TTS' => $GLOBALS['TTS'],
+                'STT' => $GLOBALS['STT'],
+                'ITT' => $GLOBALS['ITT'],
+                'FEATURES' => $GLOBALS['FEATURES']
+            ];
+        }
+
+        // Always restore from cache (both first time and subsequent times)
+        foreach ($narratorProfileCache as $key => $value) {
+            $GLOBALS[$key] = $value;
+            //$logValue = is_array($value) ? json_encode($value) : $value;
+            //minai_log("debug", "Narrator: Setting $key to $logValue");
+        }
+        
+        // Always set these after restoring cache
         $_GET["profile"] = md5("Narrator");
     }
 }
